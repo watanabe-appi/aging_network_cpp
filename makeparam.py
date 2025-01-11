0844fba61dfa97d65ec7335dd3bbd465c63b9216
H. Watanabe <kaityo@users.sourceforge.jp>
2025-01-11 19:19:57 +0900

    adds sampling

--- makeparam.py
+++ makeparam.py
@@ -15,20 +15,39 @@ percolation_sample = 100
     return filename
 
 
-def main():
+def save_param_sampling(N, alpha, beta, seed):
+    param = f"""
+system_size = {N}
+alpha = {alpha}
+beta = {beta}
+n_sample = 100
+percolation_sample = 100
+sampling = true
+data_dir = sampling
+"""
+    ia = int(alpha * 10)
+    ib = int(beta * 10)
+    filename = f"N{N:05d}_a{ia}_b{ib}.cfg"
+    print(filename)
+    with open(filename, "w") as f:
+        f.write(param)
+    return filename
+
+
+def distribution():
     N = 10000
     params = []
     params.append((-1.0, 2.0))
-    params.append(( 0.0, 2.0))
-    params.append(( 1.0, 2.0))
-    params.append(( 2.0, 2.0))
-    params.append(( 3.0, 2.0))
+    params.append((0.0, 2.0))
+    params.append((1.0, 2.0))
+    params.append((2.0, 2.0))
+    params.append((3.0, 2.0))
 
-    params.append(( 1.5, -1.0))
-    params.append(( 1.5,  0.0))
-    params.append(( 1.5,  1.0))
-    params.append(( 1.5,  2.0))
-    #params.append(( 1.5,  3.0)) //これだけ遅いので
+    params.append((1.5, -1.0))
+    params.append((1.5, 0.0))
+    params.append((1.5, 1.0))
+    params.append((1.5, 2.0))
+    # params.append(( 1.5,  3.0)) //これだけ遅いので
 
     params.append((-1.5, 2.0))
     params.append((3.0, 2.5))
@@ -41,6 +60,14 @@ def main():
     with open("task.sh", "w") as f:
         for filename in paramfiles:
             f.write(f"./aging_simulation {filename}\n")
+
+
+def sampling(alpha, beta, num):
+    N = 1000
+
+
+def main():
+    sampling(1.5, 3.0, 2)
 
 
 if __name__ == "__main__":
